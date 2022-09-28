3073bf3d91d2cd1280f3dee31b0444b5e88ddc02
fabulous1998 <fairytale_1998@163.com>
2022-09-28 15:27:30 +0800

    分页

--- onlineReading/urls.py
+++ onlineReading/urls.py
@@ -20,7 +20,7 @@ import action.views
 from . import views
 
 urlpatterns = [
-    re_path(r"^$", views.reading),
+    re_path(r"^$", views.login_page),
     path("admin/", admin.site.urls),
     path("login/", views.login, name="login"),
 
