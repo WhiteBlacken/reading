5ae1db51e72941f1180181c0636f5d2b1598804b
WhiteBlacken <48008257+WhiteBlacken@users.noreply.github.com>
2022-09-29 13:20:29 +0800

    Compute feature (#21)
    * compute reading times
    
    * save labels

--- onlineReading/views.py
+++ onlineReading/views.py
@@ -1,4 +1,5 @@
 import base64
+import json
 import math
 import os
 
@@ -30,6 +31,7 @@ from utils import (
     get_fixations,
     add_fixations_to_word,
     fixation_image,
+    reading_times,
 )
 
 
@@ -141,14 +143,22 @@ def get_page_data(request):
 
 
 def get_labels(request):
+    """一次性获得所有页的label，分页存储"""
     labels = request.POST.get("labels")
-    page = request.POST.get("page")
     experiment_id = request.session.get("experiment_id", None)
+    # 示例：labels:[{"page":1,"wordLabels":[],"sentenceLabels":[[27,57]],"wanderLabels":[[0,27]]},{"page":2,"wordLabels":[36],"sentenceLabels":[],"wanderLabels":[]},{"page":3,"wordLabels":[],"sentenceLabels":[],"wanderLabels":[[0,34]]}]
+    labels = json.loads(labels)
+
     if experiment_id:
-        PageData.objects.filter(experiment_id=experiment_id).filter(page=page).update(
-            labels=str(labels)
-        )
-    return render(request, "login.html")
+        for label in labels:
+            PageData.objects.filter(experiment_id=experiment_id).filter(
+                page=label["page"]
+            ).update(
+                wordLabels=label["wordLabels"],
+                sentenceLabels=label["sentenceLabels"],
+                wanderLabels=label["wanderLabels"],
+            )
+    return HttpResponse(1)
 
 
 def cal(request):
@@ -490,5 +500,7 @@ def get_utils_test(request):
     print("fixations:%s" % fixations)
     result = add_fixations_to_word(fixations, pagedata.location)
     username = Experiment.objects.get(id=pagedata.experiment_id).user
-    fixation_image(pagedata.image, username, fixations, page_data_id)
+    # fixation_image(pagedata.image, username, fixations, page_data_id)
+    times = reading_times(result)
+    print(times)
     return HttpResponse(result)
