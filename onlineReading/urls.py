25f017df8b4968c48558ba958bfc8aadc3a543b7
WhiteBlacken <2059164874@qq.com>
2022-10-13 11:29:13 +0800

    save work

--- onlineReading/urls.py
+++ onlineReading/urls.py
@@ -40,4 +40,5 @@ urlpatterns = [
     path("motion/", views.test_motion),
     path("save_gaze/", views.get_gaze_data_pic),
     path("heatmap/",views.get_heatmap),
+    path("visual/attention/",views.visual_attention),
 ]
