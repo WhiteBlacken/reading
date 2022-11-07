cb1d6b78bb29f9377a208476c1ae183158adcb15
fabulous1998 <fairytale_1998@163.com>
2022-11-07 11:11:55 +0800

    test

--- semantic_attention.py
+++ semantic_attention.py
@@ -10,6 +10,8 @@ from pandas import Series
 from textstat import textstat
 from transformers import BertForMaskedLM, BertTokenizer, XLNetModel, XLNetTokenizerFast
 
+from utils import get_importance
+
 nlp = spacy.load("en_core_web_lg")
 tokenizer = XLNetTokenizerFast.from_pretrained("xlnet-base-cased")
 model = XLNetModel.from_pretrained("xlnet-base-cased", output_attentions=True)
@@ -254,8 +256,8 @@ def generate_sentence_difficulty(input_text):
 
 
 if __name__ == "__main__":
-    # rst = generate_word_attention('China culture is nice culture')
-    # rst = get_importance('China culture is nice culture. Culture is important')
+    rst = generate_word_attention('China culture is nice culture')
+    rst = get_importance('China culture is nice culture. Culture is important')
     # texts = 'hello word; this is cisl.That is he.'
     # # rst = generate_sentence_attention(texts)
     # # rst = generate_word_difficulty(get_docx_text('/home/wtpan/memx4edu-code/exp_data/1009/2.docx'))
